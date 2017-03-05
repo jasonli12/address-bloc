4bc91202c9da6cecd6413783aee35752f7d1758c
Jason Li <jason.li0102@gmail.com>
2017-03-05 00:46:22 -0800

    Assignment 8 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -49,4 +49,14 @@ require "csv"
 
     return nil
   end
+
+  def iterative_search(name)
+    entries.each do |entry|
+      if name == entry.name
+        return entry
+      end
+    end
+
+    return nil
+  end
 end
